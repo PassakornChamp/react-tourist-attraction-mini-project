519a31eed9a6a5b1fbb876b6eca2b428896c8838
PassakornChamp <65725251+PassakornChamp@users.noreply.github.com>
2024-11-21 23:34:57 +0700

    TourGuide components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TravelGuide from "./TravelGuide";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      <TravelGuide />
+    </div>
+  );
 }
 
 export default App;
